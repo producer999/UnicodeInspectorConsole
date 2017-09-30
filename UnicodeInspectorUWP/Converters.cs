6de9ca33a26ec3bec6b10a0af159e36e8cd234c0
producer999 <producer999@gmail.com>
2017-09-30 17:35:05 -0400

    9/30/17 - UWP Version of the Unicode Inspector, showing the hext and int values of the entered text with copy ability for each section. The HundiSuggestBox is also included for entering Hindi words to get their Unicode values.

---
+++ UnicodeInspectorUWP/Converters.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Data;
+
+namespace UnicodeInspectorUWP
+{
+    public class StringToHexConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            string s = value as string;
+            string hexCodes = "";
+
+            foreach(char c in s)
+            {
+                int charint = (int)c;
+
+                hexCodes += "\\u" + charint.ToString("X4");
+            }
+            return hexCodes;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            return !((bool)value);
+        }
+    }
+
+    public class StringToIntConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            string s = value as string;
+            string intCodes = "";
+
+            foreach (char c in s)
+            {
+                int charint = (int)c;
+
+                intCodes += charint + " ";
+            }
+            return intCodes;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            return !((bool)value);
+        }
+    }
+}
---
+++ UnicodeInspectorUWP/MainPage.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.ApplicationModel.DataTransfer;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using sourcEleven.UWP.HindiSuggestBoxPCL;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace UnicodeInspectorUWP
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        //private double[] _fontSizes = {14, 24, 104, 164, 240};
+        //public List<double> FontSizes;
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            //FontSizes = _fontSizes.ToList();
+            //double d = FontSizes[4];
+
+            DataContext = this;
+
+            TextInput.EnableHindiIME();
+        }
+
+        private void CopyText_Button(object sender, RoutedEventArgs e)
+        {
+            if(sender is Button button)
+            {
+                DataPackage package = new DataPackage();
+                package.RequestedOperation = DataPackageOperation.Copy;
+
+                if (button.Name == "CopyHexButton")
+                {
+                    package.SetText(HexText.Text);
+                }
+                else if(button.Name == "CopyIntButton")
+                {
+                    package.SetText(IntText.Text);
+                }
+                else if(button.Name == "CopyOutputButton")
+                {
+                    package.SetText(OutputText.Text);
+                }
+
+                Clipboard.SetContent(package);
+            }
+        }
+    }
+}
